021529165bfadb10cd692f2fb65bea1fd9702441
kiki <7988388122qq.com>
2018-07-30 23:38:12 +0800

    发帖

--- Frontend/Frontend.Admin.Mobile/src/components/common/ToPost.vue
+++ Frontend/Frontend.Admin.Mobile/src/components/common/ToPost.vue
@@ -1,10 +1,11 @@
 <template>
+  <!--所有东西都以表单形式展现-->
 
 </template>
 
 <script>
     export default {
-        name: "post-page"
+        name: "to-post"
     }
 </script>
 
--- Frontend/Frontend.Admin.Mobile/src/components/page/user/PostsPage.vue
+++ Frontend/Frontend.Admin.Mobile/src/components/page/user/PostsPage.vue
@@ -1,11 +1,17 @@
 <template>
 <div>
-    <h1>user posts page</h1>
+  <h1>user posts page</h1>
+  <Post></Post>
 </div>
 </template>
 
 <script>
+    import Post from '../../common/Post.vue';
+
     export default {
+      components:{
+        Post
+      }
     }
 </script>
 
