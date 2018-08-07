a41daf188ee0947d511f99d112d92f599309121c
surevil <445073309@qq.com>
2018-08-07 21:19:54 +0800

    fix upload pool bug

--- Frontend/Frontend.Admin.Mobile/src/components/common/AdminPoolDetail.vue
+++ Frontend/Frontend.Admin.Mobile/src/components/common/AdminPoolDetail.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
     <img src="../../assets/poolname.png" style="height: 35px; width: 35px; position: absolute; top: 20px; left: 10px"/>
-    <x-input title="互选池名称"  placeholder="请输入名称"
-             style="padding: 5%; font-weight: bold; font-size: 24px; color: #464448; width: 90%; margin-left: 10%" v-model="name"/>
+    <x-input title="互选池名称" placeholder="请输入名称"
+             style="padding: 5%; font-weight: bold; font-size: 24px; color: #464448; width: 90%; margin-left: 10%"
+             v-model="name"/>
     <div label-width="4.5em" label-margin-right="2em" label-align="right">
       <datetime title="创建时间" v-model="createTime"/>
-      <selector style="height: 40px" title="城市" placeholder="请选择城市" direction="rtl" v-model="city" :options="cityList"></selector>
+      <selector style="height: 40px" title="城市" placeholder="请选择城市" direction="rtl" v-model="city"
+                :options="cityList"></selector>
       <x-input title="加入条件" text-align="right" v-model="rule"/>
       <x-textarea title="简述"/>
       <x-input :disabled="isDisable" title="互选池照片" style="color: #5d5e5c"/>
@@ -35,14 +37,14 @@
     },
     data() {
       return {
-        actionUrl: "http://127.0.0.1:5000/api/test",
+        actionUrl: "",
         url: "",
         cityList: ["苏州", "南京"],
-        name: "苏州互选池",
-        createTime: "2018年02月20日",
-        city: "苏州市",
-        rule: "年满30周岁",
-        detail: "<div style=\"text-align: center;\">【<font color=\"#990000\">活动时间</font>】</div><div style=\"text-align: center;\">dsa</div>",
+        name: "",
+        createTime: "",
+        city: "",
+        rule: "",
+        detail: "",
         isDisable: true,
       }
     },
